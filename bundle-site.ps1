a2bab82596eaa3080acf42df4ba59590e3b530ad
Will Anderson <will@itsananderson.com>
2015-06-03 15:48:43 -0700

    Use powershell instead of sh to bundle website

---
+++ bundle-site.ps1
@@ -0,0 +1,9 @@
+function ZipFiles( $zipfilename, $sourcedir )
+{
+   Add-Type -Assembly System.IO.Compression.FileSystem
+   $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+   [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
+        $zipfilename, $compressionLevel, $false)
+}
+
+ZipFiles website.zip src/
