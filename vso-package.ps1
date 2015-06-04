748945ba52ee0a45048e11bb04e9092eae591782
Will Anderson <will@itsananderson.com>
2015-06-04 10:01:06 -0700

    Add verbose logging to vso package script

--- vso-package.ps1
+++ vso-package.ps1
@@ -9,18 +9,26 @@ function ZipFiles( $zipfilename, $sourcedir )
     [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir,
         $zipfilename, $compressionLevel, $false)
 }
+
+echo "Installing base npm modules"
 cmd /C '"C:\Program Files\nodejs\npm.cmd"  i'
 
+echo "Moving package.json"
 cp package.json src/
 
+echo "Moving into src/"
 cd src/
 
+echo "Installing base production modules"
 cmd /C '"C:\Program Files\nodejs\npm.cmd" i --production'
 
+echo "Moving out of src/"
 cd ..
 
 if (test-path $zipPath) {
+    echo "Deleting old zip"
     rm $zipPath
 }
 
+echo "Creating new zip"
 ZipFiles $zipPath src/
